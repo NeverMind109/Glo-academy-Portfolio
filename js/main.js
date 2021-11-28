debe2202ff63eb4a0aba4ea812e23bd2b1788881
NeverMind109 <vladgladkih123@gmail.com>
2021-11-28 22:31:26 +0300

    Created: Added Slider for Projects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-new WOW().init();
+// WOW библиотека
+    new WOW().init();
+
+
+// Swiper slider
+const swiper = new Swiper('.projects__slider', {
+    // Optional parameters
+    loop: true,
+    // If we need pagination
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true,
+      },
+  });
